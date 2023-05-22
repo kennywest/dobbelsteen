11ae042a241f62ecf7ac4a5a678ecc1f6cb8fae9
kenneth westelinck <kenneth.westelinck@telenet.be>
2023-05-22 18:57:43 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let getal = 0
+input.onGesture(Gesture.Shake, function () {
+    getal = randint(1, 6)
+    if (getal == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else if (getal == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (getal == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (getal == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (getal == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
